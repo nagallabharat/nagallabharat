56b5c4a780123cdc16e165158836e8c06d2b6623
nagallabharat <nagallabharat@gmail.com>
2015-01-10 20:32:27 -0500

    Testing Repository

---
+++ Training-Java/src/main/java/com/practice/training/java/Training_Java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.practice.training.java.Training_Java;
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World.....Hi");
+
+	}
+
+}
